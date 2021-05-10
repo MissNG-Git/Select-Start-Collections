6b3f4f22ee7463b9b825d6dafe491a7e61874da1
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-05-10 19:53:36 +1000

    first commit -- project restart

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+// Import Packages
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const app = express();
+//----------------------------------------- END OF IMPORTS ---------------------------------------------------
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000", // <-- location of the react app were connecting to
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(cookieParser("secretcode"));
+
+//----------------------------------------- END OF MIDDLEWARE ---------------------------------------------------
+
+// Routes
+app.post("/login", (req, res) => {
+  console.log(req.body);
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+});
+
+app.get("/user", (req, res) => {
+  console.log(req.body);
+});
+//----------------------------------------- END OF ROUTES---------------------------------------------------
+
+// Start Server
+app.listen(3001, () => {
+  console.log("Server started on http://localhost:3001");
+});
