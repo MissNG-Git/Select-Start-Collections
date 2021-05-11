cb6b1335aedefaa146af0dc359e2f324d7772544
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-05-11 19:20:47 +1000

    refresh app

--- client/src/App.js
+++ client/src/App.js
@@ -1,87 +1,23 @@
-import React, { useState } from "react";
-import "./App.css";
-import axios from "axios";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerUsername, setRegisterUsername] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginUsername, setLoginUsername] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
-  // Take server data & save in state
-  const [data, setData] = useState("");
-
-  const register = () => {
-    axios({
-      method: "POST",
-      data: {
-        email: registerEmail,
-        username: registerUsername,
-        password: registerPassword,
-      },
-      withCredentials: true,
-      url: "http://localhost:3001/register",
-    }).then((res) => console.log(res));
-  };
-
-  const login = () => {
-    axios({
-      method: "POST",
-      data: {
-        username: loginUsername,
-        password: loginPassword,
-      },
-      withCredentials: true,
-      url: "http://localhost:3001/login",
-    }).then((res) => console.log(res));
-  };
-
-  const getUser = () => {
-    axios({
-      method: "GET",
-      withCredentials: true,
-      url: "http://localhost:3001/user",
-    }).then((res) => {
-      setData(res.data);
-      console.log(res.data);
-    });
-  };
-
   return (
     <div className="App">
-      <div>
-        <h1>Register</h1>
-        <input
-          placeholder="email"
-          onChange={(e) => setRegisterEmail(e.target.value)}
-        />
-        <input
-          placeholder="username"
-          onChange={(e) => setRegisterUsername(e.target.value)}
-        />
-        <input
-          placeholder="password"
-          onChange={(e) => setRegisterPassword(e.target.value)}
-        />
-        <button onClick={register}>Submit</button>
-      </div>
-      <div>
-        <h1>Login</h1>
-        <input
-          placeholder="username"
-          onChange={(e) => setLoginUsername(e.target.value)}
-        />
-        <input
-          placeholder="password"
-          onChange={(e) => setLoginPassword(e.target.value)}
-        />
-        <button onClick={login}>Submit</button>
-      </div>
-      <div>
-        <h1>Get User</h1>
-        <button onClick={getUser}>Submit</button>
-        {data ? <h1>Welcome back {data.username}</h1> : null}
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
---
+++ server.js
@@ -0,0 +1,39 @@
+// Import Packages
+const mongoose = require("mongoose");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//----------------------------------------- END OF IMPORTS ---------------------------------------------------
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+} else {
+  app.use(express.static("../client/public"));
+}
+
+// Middleware
+app.use(express.json({ extended: true }));
+app.use(cookieParser());
+
+//----------------------------------------- END OF MIDDLEWARE ---------------------------------------------------
+
+// Mongo Atlas DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/select-start-collections",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  () => {
+    console.log("Mongoose Connected!");
+  }
+);
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Back-end server listening on: http://localhost:${PORT}!`);
+});
