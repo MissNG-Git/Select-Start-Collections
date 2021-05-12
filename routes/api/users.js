a2290e24649b2b90b2f32cb26913f6dfaa995c01
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-05-12 23:42:40 +1000

    create Register endpoint & pseudo-code Login endpoint

---
+++ routes/api/users.js
@@ -0,0 +1,62 @@
+// Import dependencies
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+// Load User model
+const User = require("../../models/User");
+
+// Register endpoint
+// @route POST api/users/register || @desc Register user || @access Public
+router.post("/register", (req, res) => {
+  // Form validation
+  const { errors, isValid } = validateRegisterInput(req.body);
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+      });
+      // Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  // Form validation
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+
+  // Find user by email
+
+  // Check if user exists
+
+  // Check password
+
+  // If User matched...
+  // Create JWT Payload
+  // Sign token
+});
+
+module.exports = router;
