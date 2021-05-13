e7e465cf60d9c25665d8e87287b6e8089c150afa
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-05-13 15:45:02 +1000

    create authActions for register, login/out & import reducer to store

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,24 +1,71 @@
 // Import dependencies
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
 // Import actions from types.js
+import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 
 // Use axios to make HTTPRequests within certain action
 // Register User
-// Redirect on register??
+export const registerUser = (userData, history) => (dispatch) => {
+  axios
+    .post("/api/users/register", userData)
+    .then((res) => history.push("/login")) // Re-direct on successful register
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
 
 // Login & get user token
-// Set token to localStorage
-// Set token to authHeader
-// Decode token for user data
-// Use dispatch to send action to reducer
-// Set currentUser w/token so isAuth returns true
+export const loginUser = (userData) => (dispatch) => {
+  axios
+    .post("/api/users/login", userData)
+    .then((res) => {
+      // Set token to localStorage
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      // Set token to authHeader
+      setAuthToken(token);
+      // Decode token for user data
+      const decoded = jwt_decode(token);
+      // Use dispatch to send action to reducer
+      // Set currentUser w/token so isAuth returns true
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
 
-// Set logged in user
+// Set logged in user, pass in token
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded,
+  };
+};
 
 // User loading
+export const setUserLoading = () => {
+  return {
+    type: USER_LOADING,
+  };
+};
 
 // Log user out
-// Remove token from localStorage
-// Remove authHeader for future reqs
-// Use dispatch to send action to reducer
-// Set user to empty{} so isAuth returns false
+export const logoutUser = () => (dispatch) => {
+  // Remove token from localStorage
+  localStorage.removeItem("jwtToken");
+  // Remove authHeader for future reqs
+  setAuthToken(false);
+  // Use dispatch to send action to reducer
+  // Set user to empty{} so isAuth returns false
+  dispatch(setCurrentUser({}));
+};
--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
 // Use createStore() to hold state of app tree; use to send app state to components
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 // Set initialState
 const initialState = {};
@@ -9,11 +10,13 @@ const middleware = [thunk];
 
 // Pass empty rootReducer as 1st param, TBDefined later
 const store = createStore(
-  () => [],
+  rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
+      compose
   )
 );
 
