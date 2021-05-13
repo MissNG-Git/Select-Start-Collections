7675c3cf9a5695cb2198f3b9cbe31e3411b09d23
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-05-13 22:59:53 +1000

    add token check & implement privateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
 import "./App.css";
 
 // Check localStorage for token to keep user logged in even after browser close or page refresh
 // (e.g. until manually logging out or token expires -- 7 days); ref authActions
+if (localStorage.jwtToken) {
+  // Set token to authHeader
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token for user data
+  const decoded = jwt_decode(token);
+  // Set currentUser w/token so isAuth returns true
+  store.dispatch(setCurrentUser(decoded));
 
-// Set token to authHeader
-// Decode token for user data
-// Set currentUser w/token so isAuth returns true
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
 
-// Check for expired token
-// Logout user
-// Redirect to login
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 function App() {
   return (
@@ -28,7 +44,9 @@ function App() {
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
-          {/* Add Dashboard component & define as PrivateRoute */}
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
         </div>
       </Router>
     </Provider>
