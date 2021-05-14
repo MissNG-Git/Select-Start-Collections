86d7d980992424823bbbb0cc8fdfaa0ff75867f1
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-05-14 12:57:28 +1000

    create GameSchema

---
+++ models/Game.js
@@ -0,0 +1,20 @@
+// Import dependencies
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create GameSchema
+const GameSchema = new Schema({
+  photo: { type: String },
+  title: { type: String, required: true },
+  releaseDate: { type: Date },
+  genre: { type: String },
+  platform: { type: String },
+  synopsis: String,
+  developer: { type: String },
+  publisher: { type: String },
+  purchasePrice: { type: Number },
+  dateAdded: { type: Date, default: Date.now, required: true },
+});
+
+// Export the model
+module.exports = GameGame = mongoose.model("games", GameSchema);
